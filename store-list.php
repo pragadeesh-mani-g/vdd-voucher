3cd1e9841afc4dca3290c1cdbbdf4960d79ee2d5
Pragadeesh Mani <passionpraga397@gmail.com>
2018-08-14 18:29:00 +0530

    footer

--- store-list.php
+++ store-list.php
@@ -249,7 +249,7 @@
             <footer class="footer container-fluid pl-30 pr-30">
                 <div class="row">
                     <div class="col-sm-12">
-                        <p>2017 &copy; Hound. Pampered by Hencework</p>
+                        <p> <a href="http://www.viswadevji.in/" target="_blank">Viswa And Devji</a>© Copyright 2018 | All Rights Rerved | Designed & Developed By <a href="http://episodetechnologies.com/" target="_blank">Episode Technologies</a></p>
                     </div>
                 </div>
             </footer>
